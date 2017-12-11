8d4e1fe87fb670d84c386539f2c2ec08943615ec
ChristosGui88 <christosathens@hotmail.gr>
2017-12-11 22:48:14 +0000

    Importing Project

---
+++ CrimeWatch/Controllers/MapController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace CrimeWatch.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            ViewBag.CurrentAddress = "";
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult FindLocation(String postcode) {
+            
+            string requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?postcode={0}&sensor=false", Uri.EscapeDataString(postcode));
+            
+            WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
+            XDocument xdoc = XDocument.Load(response.GetResponseStream());
+
+            XElement result = xdoc.Element("GeocodeResponse").Element("result");
+            XElement locationElement = result.Element("geometry").Element("location");
+            XElement lat = locationElement.Element("lat");
+            XElement lng = locationElement.Element("lng");
+
+            ViewBag.latitude = (int)lat;
+            ViewBag.longitude = (int)lng;
+
+            return View("Map");
+        }
+    }
+}
---
+++ CrimeWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrimeWatch.Startup))]
+namespace CrimeWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
